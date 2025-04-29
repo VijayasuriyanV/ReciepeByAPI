09db106cf52d4d6b0413592d82706a84ba61bee1
Jeya Chandran <jeyachandranjcs@gmail.com>
2025-04-29 17:16:29 +0530

    home comp completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import Home from './Pages/Home'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-       
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/Components/Card.tsx
@@ -0,0 +1,41 @@
+import { Recipe } from "../types/reciepetypes"
+import "../styles/Card.css"
+
+
+interface Props{
+    data:Recipe[] | null,
+    error:string| null
+}
+export default function Card({data,error}:Props) {
+  return (
+    <div className="container">
+       {error ? <p>{error}</p> : 
+        <div className="card-wrapper">
+        {data?.map((item)=>(
+            <div className="card" key={item.id}>
+                <img className="card-img" src={item.image}/>
+                <br />
+
+                <div className="card-details">
+                    <h3>{item.name}</h3>
+                    <p>{item.rating}</p>
+                </div>
+
+                <div className="card-info">
+                    <p><i className="fa-solid fa-clock"></i>{item.cookTimeMinutes}</p>
+                    <p>Servings: {item.servings}</p>
+                    <p>{item.cuisine}</p>
+                </div>
+
+                <div className="card-footer">
+                    <p>Review-count: {item.reviewCount}</p>
+                    <button>Full Recipe</button>
+                </div>
+
+            </div>
+        ))}
+    </div>
+       }
+    </div>
+  )
+}
