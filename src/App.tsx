597d657628c56ec3078ab951715b81618b391515
VijayasuriyanV <vijayasuriyanvenkat@gmail.com>
2025-04-29 18:11:44 +0530

    Frst day final commit

--- src/App.tsx
+++ src/App.tsx
@@ -18,15 +18,18 @@ import ReciepeDetails from "./Pages/ReciepeDetails";
 
 import Home from "./Pages/Home";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
-
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipe/:id" element={<ReciepeDetails />} />
       </Routes>
+      <Footer />
     </Router>
 >>>>>>> e27f97d36af37c05225ae27aaa6af825bda62928
   );
