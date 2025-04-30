e02f92cfd4c59bf58015d45a7761f74e2a793cab
VijayasuriyanV <vijayasuriyanvenkat@gmail.com>
2025-04-30 10:55:44 +0530

    Banner implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import SlidingBanner from "./Components/SlidingBanner";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <SlidingBanner />
-    </>
-=======
 // import "./App.css";
 import ReciepeDetails from "./Pages/ReciepeDetails";
 
@@ -20,18 +5,19 @@ import Home from "./Pages/Home";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import SlidingBanner from "./Components/SlidingBanner";
 
 function App() {
   return (
     <Router>
       <Header />
+      <SlidingBanner />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipe/:id" element={<ReciepeDetails />} />
       </Routes>
       <Footer />
     </Router>
->>>>>>> e27f97d36af37c05225ae27aaa6af825bda62928
   );
 }
 
